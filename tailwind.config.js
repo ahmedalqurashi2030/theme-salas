23a532ac3cb1be2b32a611360bda98a3cab933fa
sakhr <dev.sakhr@gmail.com>
2026-02-05 01:21:10 +0300

    New Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -139,6 +139,6 @@ module.exports = {
     plugins: [
         require('@salla.sa/twilight-tailwind-theme'),
         require('@tailwindcss/forms'),
-        // Note: @tailwindcss/line-clamp removed - built into Tailwind v3.3+
+        require('@tailwindcss/line-clamp'),
     ],
 }
