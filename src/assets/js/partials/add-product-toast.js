fc1d7afee1ac30663ed4c96cabb44de2842b5117
sakhr <dev.sakhr@gmail.com>
2026-02-16 05:36:12 +0300

    New Changes

---
+++ src/assets/js/partials/add-product-toast.js
@@ -0,0 +1,3 @@
+// Placeholder for add-product-toast.js
+// This file was missing and created to fix the build error.
+console.log('add-product-toast.js loaded');
---
+++ src/assets/js/partials/digital-files.js
@@ -0,0 +1,3 @@
+// Placeholder for digital-files.js
+// This file was missing and created to fix the build error.
+console.log('digital-files.js loaded');
